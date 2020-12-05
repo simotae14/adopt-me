340dd336dd8c8aa41005539e1cd5dda59e063018
Simone <startae14@gmail.com>
2020-12-05 17:00:28 +0100

    9. create a configuration file for prettier to run the default rules with one added by me

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
+  return React.createElement('div', { id: 'something-important' }, [
+    React.createElement('h1', {}, 'Adopt Me!'),
     React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
     }),
     React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cocktail",
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cocktail',
     }),
     React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
+      name: 'Doink',
+      animal: 'Cat',
+      breed: 'Mixed',
     }),
   ]);
 };
@@ -31,5 +31,5 @@ const App = () => {
 ReactDOM.render(
   React.createElement(App),
   // where to render
-  document.getElementById("root")
+  document.getElementById('root')
 );
