34221c58e7f961e58f24a5fd5db1b289a80283fa
Simone <startae14@gmail.com>
2020-12-06 00:59:27 +0100

    23. create a breed Dropdown disabled because is dependent on the animal selection

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -5,6 +5,9 @@ import { ANIMALS } from '@frontendmasters/pet';
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [animal, setAnimal] = useState('dog');
+  const [breed, setBreed] = useState();
+  const [breeds, setBreeds] = useState([]);
+
   return (
     <div className="search-params">
       <h1>{location}</h1>
@@ -30,6 +33,23 @@ const SearchParams = () => {
             {ANIMALS.map((animal) => (
               <option key={animal} value={animal}>
                 {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onBlur={(e) => setBreed(e.target.value)}
+            onChange={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
               </option>
             ))}
           </select>
