aa18f9ef9ab3200d9b1e314397313511979b6951
Simone <startae14@gmail.com>
2020-12-05 18:16:43 +0100

    13. install react and react-dom as dependencies and use them instead of cdns inside your project and your components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const Pet = ({ name, animal, breed }) => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, name),
@@ -27,8 +30,8 @@ const App = () => {
   ]);
 };
 
-// render in the browser
-ReactDOM.render(
+// render in the browser with ReactDOM
+render(
   React.createElement(App),
   // where to render
   document.getElementById('root')
