c6f63635afcb1cac1c50a6f1730969a5e0df1bfc
Simone <startae14@gmail.com>
2020-12-06 18:19:34 +0100

    29. create the Results component with all the results of the api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import pet, { ANIMALS } from '@frontendmasters/pet';
+import Results from './Results';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -53,6 +54,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
