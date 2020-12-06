bc0817bd2186b7925df9789eb8fee5e20d250ea7
Simone <startae14@gmail.com>
2020-12-06 15:15:38 +0100

    26. wrap your App component to React.Strict mode

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SearchParams from './SearchParams';
 
 const App = () => {
   return (
-    <div id="something-important">
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div id="something-important">
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
