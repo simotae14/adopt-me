04aefa9b7ff76471ae8847d51c5bd0b75522bbda
Simone <startae14@gmail.com>
2020-12-06 19:01:10 +0100

    31. install Reach Router for routing and add the Router wrapper to the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router } from '@reach/router';
 
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
   return (
     <React.StrictMode>
       <div id="something-important">
         <h1>Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
