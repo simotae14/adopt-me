fff11ac1f1972444b3861a4952fcf3ebb22f4c85
Simone <startae14@gmail.com>
2020-12-14 19:52:26 +0100

    5. create an animation with emotion using the keyframes method

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 // import
 import colors from './colors';
 
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
 const NavBar = () => {
-  const [padding, setPadding] = useState(15);
   return (
     <header
-      onClick={() => setPadding(padding + 15)}
       css={css`
         background-color: ${colors.dark};
         position: sticky;
@@ -20,8 +24,11 @@ const NavBar = () => {
       <span
         css={css`
           font-size: 60px;
+          display: inline-block;
+          animation: 0.1s ${spin} linear infinite;
           &:hover {
             text-decoration: underline;
+            animation: 1s ${spin} linear infinite reverse;
           }
         `}
         aria-label="logo"
